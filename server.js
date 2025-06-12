5d74ee2d43eb349ee0e868156dfb0c4b6b0250ce
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-06-12 10:36:53 +0530

    Update server port from 3000 to 3001

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
